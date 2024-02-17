e56b1da36fc4de21503bda66ddfbc0bcdb973c0b
surendiran04 <137646072+surendiran04@users.noreply.github.com>
2024-02-17 21:04:04 +0530

    Update 349.intersection of two arrays.cpp

--- Easy/349.intersection of two arrays.cpp
+++ Easy/349.intersection of two arrays.cpp
@@ -14,3 +14,18 @@ public:
         return s3;
     }
 };
+//more efficient code
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        set<int> set1(nums1.begin(),nums1.end());
+        vector<int> result;
+        for(int num :nums2){
+                if(set1.count(num)){
+                    result.push_back(num);
+                    set1.erase(num);
+                }
+        }
+        return result;
+    }
+};
