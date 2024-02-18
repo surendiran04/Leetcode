b18b69ea3763a55e22c4f457fb6694a3875d0d9c
surendiran04 <137646072+surendiran04@users.noreply.github.com>
2024-02-18 23:43:13 +0530

    efficient method added

--- Easy/231.Power of 2.cpp
+++ Easy/231.Power of 2.cpp
@@ -5,6 +5,15 @@ public:
         if(n==0){
             return false;
         }
+        //floor returns the min nearest integer - floor(86.34) = 86
+        //ceil returns the max nearest integer - ceil(86.34) = 87
         return (ceil(log2(n))==floor(log2(n)));
+    }
+};
+//by bit manupulation method which more faster
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        return n>0 && !(n & n-1);  //(n & n-1) - checks for common bits, // 5 & 4 = 4, // 4 & 3 = 0
     }
 };
