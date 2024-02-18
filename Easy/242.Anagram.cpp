19f39c1ec695032e53b587211bae11e1688036a7
surendiran04 <137646072+surendiran04@users.noreply.github.com>
2024-02-19 01:35:07 +0530

    Added reduced time complexity code

--- Easy/242.Anagram.cpp
+++ Easy/242.Anagram.cpp
@@ -1,5 +1,5 @@
 class Solution {
-public:
+public:                                                // time - O(nlogn);
     bool isAnagram(string s, string t) {
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
@@ -8,5 +8,28 @@ public:
         }
         return false;
 
+    }
+};
+//using hash map checking all the letters size and reducing it to zero - O(n)
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+         unordered_map<char, int> count;
+
+        for (auto x : s) {
+            count[x]++;
+        }
+        
+        for (auto x : t) {
+            count[x]--;
+        }
+        // Check if any character has non-zero frequency
+        for (auto x : count) {
+            if (x.second != 0) {
+                return false;
+            }
+        }
+        
+        return true;
     }
 };
