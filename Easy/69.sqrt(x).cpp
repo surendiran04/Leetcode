1f825311756ee95a19f0e6a2f62346364b1f97fb
surendiran04 <137646072+surendiran04@users.noreply.github.com>
2024-02-22 23:49:39 +0530

    Square of a no using Binary search

---
+++ Easy/69.sqrt(x).cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int low=0;
+        int high=x;
+        long long mid;
+        while(low<=high){
+            mid=(low+high)/2;
+            if(mid*mid==x) return mid;
+            else if(mid*mid>x) high= mid-1;
+            else low=mid+1;
+        }
+        return high;
+    }
+};
