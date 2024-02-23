87e9a0494527c1a893511ef4942c286b801f7af0
surendiran04 <137646072+surendiran04@users.noreply.github.com>
2024-02-23 23:05:20 +0530

    Update 69.sqrt(x).cpp

--- Easy/69.sqrt(x).cpp
+++ Easy/69.sqrt(x).cpp
@@ -13,3 +13,19 @@ public:
         return high;
     }
 };
+//100% efficient code
+class Solution {
+public:
+    int mySqrt(int x) {
+        int low=0;
+        int high=x;
+        long long mid;
+        while(low<=high){
+            mid=low+(high-low)/2;  //this condition is important
+            if(mid*mid==x) return mid;
+            else if(mid*mid>x) high= mid-1;
+            else low=mid+1;
+        }
+        return high;
+    }
+};
