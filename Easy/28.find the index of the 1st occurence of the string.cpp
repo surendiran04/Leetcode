9e182b015418cff484c60fc9c8d5ef2ce7fb8586
surendiran04 <137646072+surendiran04@users.noreply.github.com>
2024-02-25 00:19:01 +0530

    Update 28.find the index of the 1st occurence of the string.cpp

--- Easy/28.find the index of the 1st occurence of the string.cpp
+++ Easy/28.find the index of the 1st occurence of the string.cpp
@@ -4,6 +4,7 @@ public:
         if (needle.length() > haystack.length()) {
             return -1;
         }
+        //time complexity - O(n*m) we can also use KMP algo whic of time O(n+m)
         for (int i = 0; i <= haystack.length() - needle.length(); ++i) {
             if (haystack.substr(i, needle.length()) == needle) { //substr fn creates a substring to the length of the needle,i is the starting index of the substring
                 return i;
