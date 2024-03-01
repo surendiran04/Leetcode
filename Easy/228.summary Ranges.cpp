2a5846980166546e38308f19b175ef3900684519
surendiran04 <137646072+surendiran04@users.noreply.github.com>
2024-03-01 21:05:28 +0530

    Update 228.summary Ranges.cpp

--- Easy/228.summary Ranges.cpp
+++ Easy/228.summary Ranges.cpp
@@ -8,13 +8,13 @@ public:
         int a = nums[0];
         for(int i = 0; i<n; i++)
         {
-            if( i == n-1 || nums[i]+1 != nums[i+1])
+            if( i == n-1 || nums[i]+1 != nums[i+1]) //checking if element's is a non-consecutive one
             {
-                if(nums[i] != a)
+                if(nums[i] != a) //stores the consecutive range
                     result.push_back(to_string(a)+ "->"+ to_string(nums[i]));
-                else
-                        result.push_back(to_string(a));
-                if(i != n-1)
+                else //if it is a non-consecutive one then it'll be stored 
+                        result.push_back(to_string(a)); 
+                if(i != n-1) 
                     a = nums[i+1];
             }
         }
