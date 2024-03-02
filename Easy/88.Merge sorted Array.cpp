a689558681bbe5201ade73524839382495bca63a
surendiran04 <137646072+surendiran04@users.noreply.github.com>
2024-03-02 22:30:03 +0530

    Update 88.Merge sorted Array.cpp

--- Easy/88.Merge sorted Array.cpp
+++ Easy/88.Merge sorted Array.cpp
@@ -11,3 +11,14 @@ public:
         sort(nums1.begin(),nums1.end());
     }
 };
+//both the codes are 100% efiicient 
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        for (int j = 0, i = m; j<n; j++){
+            nums1[i] = nums2[j];
+            i++;
+        }
+        sort(nums1.begin(),nums1.end());
+    }
+};
