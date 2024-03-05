bc21ef5d0dec56ff2f2b4bbb473c289114a050fb
surendiran04 <137646072+surendiran04@users.noreply.github.com>
2024-03-05 16:50:42 +0530

    Update 2798.Number of employees who met the target.cpp

--- Easy/2798.Number of employees who met the target.cpp
+++ Easy/2798.Number of employees who met the target.cpp
@@ -10,3 +10,16 @@ public:
         return count;
     }
 };
+//foreach loop is speeder than normal for loop
+class Solution {
+public:
+    int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
+        int count=0;
+        for(int i=0;i<hours.size();i++){
+            if(hours[i]>=target){
+                count++;
+            }
+        }
+        return count;
+    }
+};
