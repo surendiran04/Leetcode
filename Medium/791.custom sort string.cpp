86b5e270903889d9f01b29d7869155536a58d991
surendiran04 <137646072+surendiran04@users.noreply.github.com>
2024-03-11 14:33:38 +0530

    Solution in map DS and Vector approach

--- Medium/791.custom sort string.cpp
+++ Medium/791.custom sort string.cpp
@@ -44,3 +44,27 @@ public:
         return ans;
     }
 };
+//another easy logic from leetcode soln
+class Solution {
+public:
+    string customSortString(string order, string s) {
+        string custom;
+        vector<int> freq(26, 0);
+        for (char c: s) {
+            freq[c -'a']++;
+        }
+        for (char c: order) {
+            while (freq[c -'a']) { //char-'a' gives the index between 0 and 26 coz, asciiz value a is 97 and c is 99 so 99-92=2
+                custom += c;
+                freq[c -'a']--;
+            }
+        }
+        for (int i = 0; i < 26; i++) {
+            while (freq[i]) {
+                custom += i + 'a';
+                freq[i]--;
+            }
+        }
+        return custom;
+    }
+};
