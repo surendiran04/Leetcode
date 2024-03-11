9cf2c3cb32242bfcc7061343f4a2f8de95812e69
surendiran04 <137646072+surendiran04@users.noreply.github.com>
2024-03-11 19:20:31 +0530

    Create 342.Power of four.cpp

---
+++ Easy/342.Power of four.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        while(n>1){
+            if(n%4!=0){
+                return false;
+            }
+            n=n/4;
+        }
+        if(n==1){ //if it is divided by 4 then it gives 1 as quotient
+            return true;
+        }
+        return false;
+    }
+};
