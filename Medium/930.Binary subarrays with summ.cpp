0cabf403257b73769c7be66aa110ae31a8d0874e
surendiran04 <137646072+surendiran04@users.noreply.github.com>
2024-03-15 00:33:01 +0530

    Sliding window concept

--- Medium/930.Binary subarrays with summ.cpp
+++ Medium/930.Binary subarrays with summ.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
         unordered_map<int, int> count;
-        count[0] = 1;
+        count[0] = 1; //intially count[0]=1 coz,if the curr_sum-goal becomes 0 we got the subarray sum = goal so weed need to add 1 to the total_subarray count
         int curr_sum = 0;
         int total_subarrays = 0;
 
         for (int num : nums) {
-            curr_sum += num;
-            if (count.find(curr_sum - goal) != count.end()) {
+            curr_sum += num;      //here we are keep on adding the sum
+            if (count.find(curr_sum - goal) != count.end()) { //if the curr_sum - goal found in the map that means there exist a subarray whose sum is equal to goal
                 total_subarrays += count[curr_sum - goal];
             }
             count[curr_sum]++;
