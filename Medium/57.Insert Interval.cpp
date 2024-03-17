48679dc1e2280336f30b62cfd3f4ef626685343a
surendiran04 <137646072+surendiran04@users.noreply.github.com>
2024-03-17 18:09:54 +0530

    Solved using 1-forloop and 3-whileloop approach

--- Medium/57.Insert Interval.cpp
+++ Medium/57.Insert Interval.cpp
@@ -30,3 +30,29 @@ public:
          return ans;
     }
 };
+//1st code is more optimised approach
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> merged;
+        int i = 0;
+
+        while (i < intervals.size() && intervals[i][1] < newInterval[0]) { //when the newInterval doesn't comes within the range of the current interval
+            merged.push_back(intervals[i]);
+            i++;
+        }
+
+        while (i < intervals.size() && intervals[i][0] <= newInterval[1]) {
+            newInterval = {min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])}; 
+            i++;
+        }
+        merged.push_back(newInterval);
+
+        while (i < intervals.size()) { //after the newInterval is inserted in its crt position
+            merged.push_back(intervals[i]);
+            i++;
+        }
+
+        return merged;
+    }
+};
