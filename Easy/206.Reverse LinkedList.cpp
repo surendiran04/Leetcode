dafbe1cba047159556b3f073f0e289d91f482c50
surendiran04 <137646072+surendiran04@users.noreply.github.com>
2024-03-21 18:36:36 +0530

    Two sliding pointer appproach

---
+++ Easy/206.Reverse LinkedList.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* p=head;
+        ListNode* q=NULL;
+        ListNode* r=NULL;
+        while(p){
+            r=q;
+            q=p;
+            p=p->next;
+            q->next=r;
+        }
+        return q;
+    }
+};
