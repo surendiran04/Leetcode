5126535a35c56f636c7a385d4a74a595a67f8348
surendiran04 <137646072+surendiran04@users.noreply.github.com>
2024-03-22 09:28:43 +0530

    Sliding two pointer approach || nullptr changed

--- Easy/206.Reverse LinkedList.cpp
+++ Easy/206.Reverse LinkedList.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
         ListNode* p=head;
-        ListNode* q=NULL;
+        ListNode* q=NULL; //It is preferred to use nullptr instead of NULL which avoids ambiquity associated with NULL
         ListNode* r=NULL;
         while(p){
             r=q;
