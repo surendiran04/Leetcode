8f314c459175e00f85ce65affb30c633d6a91b07
surendiran04 <137646072+surendiran04@users.noreply.github.com>
2024-03-24 11:25:22 +0530

    Floyd's tortoise & Hare algo and map frequency

--- Medium/287.Find the duplicate number.cpp
+++ Medium/287.Find the duplicate number.cpp
@@ -30,6 +30,32 @@ public:
             if(map[num]>1){
                 return num;
             }
+             return 0;
+    }
+};
+ //using Floyd’s Tortoise and Hare algorithm           
+class Solution {  //uses O(1) space
+public:
+    int findDuplicate(std::vector<int>& nums) {
+        int slow = nums[0]; 
+        int fast = nums[0];
+
+        // Find the intersection point of the two pointers
+        do {
+            slow = nums[slow]; //nums[first index] gives the other numbers value,by this way both fast and slow are changing
+            fast = nums[nums[fast]]; //fast is moving at 2x faster time than slow
+        } while (slow != fast);
+
+        // Find the entrance of the cycle
+        slow = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+    }
+};
         }
         return 0;
     }
