12480139efcebee59780a1c5c78ac2763633b1be
surendiran04 <137646072+surendiran04@users.noreply.github.com>
2024-03-25 17:35:03 +0530

    Update 2864.Maximum odd binary number.cpp

--- Easy/2864.Maximum odd binary number.cpp
+++ Easy/2864.Maximum odd binary number.cpp
@@ -8,3 +8,18 @@ public:
         return s;
     }
 };
+//both are the codes are efficient
+class Solution {
+public:
+    string maximumOddBinaryNumber(string s) {
+        int n1 = 0;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='1'){
+                n1++;
+            }
+        }
+        int n =s.length()-n1;
+        string ans=string(n1-1,'1')+string(n,'0')+'1';
+        return ans;
+    }
+};
