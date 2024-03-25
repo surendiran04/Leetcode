d0240a49dddfb9aca97f4060df68d2ccd0bba7a2
surendiran04 <137646072+surendiran04@users.noreply.github.com>
2024-03-25 17:35:36 +0530

    Update 2864.Maximum odd binary number.cpp

--- Easy/2864.Maximum odd binary number.cpp
+++ Easy/2864.Maximum odd binary number.cpp
@@ -1,3 +1,14 @@
+//this code partially fails for high test cases where the stoi fn cannot able to covert the string to number
+class Solution {
+public:
+    string maximumOddBinaryNumber(string s) {
+        int n1 = __builtin_popcount(stoi(s,nullptr,2));
+        int n =s.length()-n1;
+        string ans=string(n1-1,'1')+string(n,'0')+'1';
+        return ans;
+    }
+};
+------------------------------------------------------------
 class Solution {
 public:
     string maximumOddBinaryNumber(string s) {
