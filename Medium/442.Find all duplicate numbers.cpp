b6649075cdadf8af2e81429e844cb26f06feadfa
surendiran04 <137646072+surendiran04@users.noreply.github.com>
2024-03-25 18:22:48 +0530

    Negative the value and checking the number is repeating again

--- Medium/442.Find all duplicate numbers.cpp
+++ Medium/442.Find all duplicate numbers.cpp
@@ -13,3 +13,20 @@ public:
 
     }
 };
+//second approach uses less space
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int>result;
+        int n = nums.size();
+        for(int i=0; i<n; i++){
+            int num =abs(nums[i]);
+            int index = num - 1;
+            if(nums[index] < 0)  //negating the value if the index again occurs then the nums[index] will be less than 0 and added to the result 
+                result.push_back(num);
+            
+            nums[index] *= -1;
+        }
+        return result;
+    }
+};
