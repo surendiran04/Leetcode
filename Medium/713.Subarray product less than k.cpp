c52f379ecf0eb2d7faecb1908e0fecd906b6bd9c
surendiran04 <137646072+surendiran04@users.noreply.github.com>
2024-03-29 12:06:44 +0530

    Two pointer approach with sliding window

--- Medium/713.Subarray product less than k.cpp
+++ Medium/713.Subarray product less than k.cpp
@@ -2,6 +2,9 @@ class Solution {
 public:
     int numSubarrayProductLessThanK(vector<int>& nums, int k) {
         int count=0,j=0;int product=1;
+        if(k<=1){
+            return 0; //if k is 0/1 then no subarray is possible
+        }
         for(int i=0;i<nums.size();i++){
              product =product * nums[i];
             while(product>=k && j<=i){
