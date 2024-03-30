ffee6e988642ff27278b56fab026b002acca1011
surendiran04 <137646072+surendiran04@users.noreply.github.com>
2024-03-30 17:02:28 +0530

    Create 992. Subarrays with K Different Integers.cpp

---
+++ Hard/992. Subarrays with K Different Integers.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        return countSubarraysWithAtMostKDistinct(nums, k) - countSubarraysWithAtMostKDistinct(nums, k - 1);
+    }
+
+    int countSubarraysWithAtMostKDistinct(vector<int>& nums, int k) {
+        int start = 0;
+        int count = 0;
+        unordered_map<int, int> frequencyMap;
+        for (int end = 0; end < nums.size(); end++) {
+            frequencyMap[nums[end]]++;
+            while (frequencyMap.size() > k) {
+                frequencyMap[nums[start]]--;
+                if (frequencyMap[nums[start]] == 0) {
+                    frequencyMap.erase(nums[start]);
+                }
+                start++;
+            }
+            count += end - start + 1;
+        }
+        return count;
+    }
+};
+
+
