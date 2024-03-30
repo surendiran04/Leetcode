460b811e2e7faa2bc55300f4dd845407e0d366c9
surendiran04 <137646072+surendiran04@users.noreply.github.com>
2024-03-30 18:07:32 +0530

    Subtracting at most k frequency with k-1 frequency || 2958.Same logic

--- Hard/992. Subarrays with K Different Integers.cpp
+++ Hard/992. Subarrays with K Different Integers.cpp
@@ -4,7 +4,7 @@ public:
         return countSubarraysWithAtMostKDistinct(nums, k) - countSubarraysWithAtMostKDistinct(nums, k - 1);
     }
 
-    int countSubarraysWithAtMostKDistinct(vector<int>& nums, int k) {
+    int countSubarraysWithAtMostKDistinct(vector<int>& nums, int k) { //this fn is the exact code of 2958.Length of longest subarray with at most k grequency
         int start = 0;
         int count = 0;
         unordered_map<int, int> frequencyMap;
