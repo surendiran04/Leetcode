86c6b2ee7f65f63a3157d373a6bbb901c1f791e4
surendiran04 <137646072+surendiran04@users.noreply.github.com>
2024-04-02 18:09:30 +0530

    Update 1207.Unique number of occurences.cpp

--- Easy/1207.Unique number of occurences.cpp
+++ Easy/1207.Unique number of occurences.cpp
@@ -15,3 +15,21 @@ public:
         return true;
     }
 };
+//the 1st approach is faster
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int,int> occurrencesMap;
+        for (const auto& i: arr) {
+            occurrencesMap[i]++;
+        }
+        unordered_set<int> uniqueCountsSet;
+        for (const auto& entry: occurrencesMap) {
+            if (!uniqueCountsSet.insert(entry.second).second) { //the insert fn returns two values: first-inserted element's position an                                                                  
+                 return false;
+            }
+        }
+
+        return true;
+    }
+};
