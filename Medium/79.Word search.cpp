d0249789d70977b4b583999a73ab239c72447140
surendiran04 <137646072+surendiran04@users.noreply.github.com>
2024-04-03 19:14:16 +0530

    Backtracking | DFS | Need to be revised later

--- Medium/79.Word search.cpp
+++ Medium/79.Word search.cpp
@@ -122,4 +122,44 @@ public:
     }
 };
 
+//quite looks easy
+#include <vector>
+#include <string>
+
+class Solution {
+public:
+    bool exist(std::vector<std::vector<char>>& board, std::string word) {
+        int rows = board.size();
+        int cols = board[0].size();
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (solve(board, rows, cols, i, j, word, 0)) return true;
+            }
+        }
+        return false;
+    }
+
+private:
+    std::vector<int> rowDirections = {1, -1, 0, 0};
+    std::vector<int> colDirections = {0, 0, 1, -1};
+
+    bool solve(std::vector<std::vector<char>>& board, int rows, int cols, int row, int col, const std::string& word, int index) {
+        if (index == word.length()) return true;
+        if (row < 0 || row >= rows || col < 0 || col >= cols || board[row][col] != word[index]) return false;
+        char temp = board[row][col];
+        board[row][col] = '.';
+
+        for (int d = 0; d < 4; d++) {
+            int newRow = row + rowDirections[d];
+            int newCol = col + colDirections[d];
+            if (solve(board, rows, cols, newRow, newCol, word, index + 1)) return true;
+        }
+
+        board[row][col] = temp;
+        return false;
+    }
+};
+
+
 
