23ac2610029fa75131b81551fe4c53a05b9aa35b
surendiran04 <137646072+surendiran04@users.noreply.github.com>
2024-04-05 00:27:41 +0530

    String | parantheses

--- Easy/1614.Maximum nesting depth of the paranthesis.cpp
+++ Easy/1614.Maximum nesting depth of the paranthesis.cpp
@@ -3,10 +3,10 @@ public:
     int maxDepth(string s) {
         int count = 0;
         int max_num = 0;
-        for (char c : s) {
+        for (char c : s) {               // whenever encounter a '(' increase the count and decrease the count whenever ')'
             if (c == '(') {
                 count++;
-                if (max_num < count)
+                if (max_num < count)   //changing the max_num whenever the count is greater
                     max_num = count;
             } else if (c == ')') {
                 count--;
