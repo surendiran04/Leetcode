1b7017236f635174f5480137f13e88226a0ed5c2
surendiran04 <137646072+surendiran04@users.noreply.github.com>
2024-04-05 13:01:11 +0530

    Update 1544.Make the string great.cpp

--- Medium/1544.Make the string great.cpp
+++ Medium/1544.Make the string great.cpp
@@ -17,3 +17,19 @@ public:
         return ans;
     }
 };
+//another best approach
+class Solution {
+public:
+    bool check(char x, char y) {
+        return abs(x - y) == 32;
+    }
+
+    string makeGood(string s) {
+        string res = "";
+        for (char c : s) {
+            if (!res.empty() && check(res.back(), c)) res.pop_back();
+            else res += c;
+        }
+        return res;
+    }
+};
