9d2757de32dca40e11dabe721196b2fc1b14d66a
surendiran04 <137646072+surendiran04@users.noreply.github.com>
2024-04-08 22:31:33 +0530

    Create 1700.Number of students unable to eat lunch.cpp

---
+++ Easy/1700.Number of students unable to eat lunch.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+        queue<int> s1; 
+        queue<int> s2; 
+
+        for (int student : students) {
+            s1.push(student);
+        }
+        for (int sandwich : sandwiches) {
+            s2.push(sandwich);
+        }
+        int i = 0;
+        while (!s1.empty() && i  < s2.size()) {
+            if (s1.front() == s2.front()){  //if both 1st elements are equal then pop the 1st elements
+                s1.pop();
+                s2.pop();
+                i=0;      //i is the key here,if none of the student take the 1st sandwich then loop will end
+            } else {
+                int student = s1.front();
+                s1.pop();
+                s1.push(student);
+                i++;
+            }
+        }
+        return s1.size();
+    }
+};
