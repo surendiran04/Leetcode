0967046d144411a51490e50b89d28426f90c0472
surendiran04 <137646072+surendiran04@users.noreply.github.com>
2024-04-12 19:09:12 +0530

    two pointer | Find the High between the 2 walls height

--- Hard/42.Trapping Rain water.cpp
+++ Hard/42.Trapping Rain water.cpp
@@ -23,3 +23,23 @@ public:
         return trappedWater;
     }
 };
+//beats 66% - the same above approach is optimised both in time and space
+class Solution {
+public:
+    int trap(std::vector<int>& height) {
+        int i = 0, left_max = height[0], sum = 0;
+        int j = height.size() - 1, right_max = height[j];
+        while (i < j) {
+            if (left_max <= right_max) {
+                sum += (left_max - height[i]);
+                i++;
+                left_max = std::max(left_max, height[i]);
+            } else {
+                sum += (right_max - height[j]);
+                j--;
+                right_max = std::max(right_max, height[j]);
+            }
+        }
+        return sum;
+    }
+};
