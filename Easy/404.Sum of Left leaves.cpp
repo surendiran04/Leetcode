1003bb9d0c9e84150b1c0b25ff0cdadeec9f4498
Surendiran <137646072+surendiran04@users.noreply.github.com>
2024-04-14 09:59:15 +0530

    BFS | DFS | RECURSIVE APPROACH

--- Easy/404.Sum of Left leaves.cpp
+++ Easy/404.Sum of Left leaves.cpp
@@ -1,3 +1,24 @@
+class Solution {
+public:
+   int sumOfLeftLeaves(TreeNode* root) { 
+     // Check if the left child of the root exists:
+           //1.If the left child exists and it is a leaf node (i.e., it has no children), add its value to ans.
+           //2.If the left child exists and it is not a leaf node, recursively call the sumOfLeftLeaves function for the left child.
+      //Recursively call the sumOfLeftLeaves function for the right child of the root.
+        if (!root)
+            return 0;
+        int ans = 0; 
+        if (root->left) {
+            if (!root->left->left && !root->left->right)  
+                ans += root->left->val;
+            else
+                ans += sumOfLeftLeaves(root->left);
+        }
+        
+        ans += sumOfLeftLeaves(root->right);
+        return ans;
+   }
+};
 class Solution { //BFS approach quite time and space consuming
 public:
    int sumOfLeftLeaves(TreeNode* root) {
