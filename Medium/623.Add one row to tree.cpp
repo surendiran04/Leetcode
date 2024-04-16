e5422f45fc611d5f74465ac7fce0a0bdb502fe8c
Surendiran <137646072+surendiran04@users.noreply.github.com>
2024-04-16 18:37:22 +0530

    DFS | tree

--- Medium/623.Add one row to tree.cpp
+++ Medium/623.Add one row to tree.cpp
@@ -32,3 +32,32 @@ public:
         return add(root, val, depth, 1);
     }
 };
+class Solution { //recursive dfs approach beats 90%
+public:
+    void dfs(TreeNode* root, int val, int depth, int level){
+        if (!root) return;
+        if (level<depth-1){
+            dfs(root->left, val, depth, level+1);
+            dfs(root->right, val, depth, level+1);
+        }
+        else{
+            TreeNode* ptr=root->left;
+            root->left=new TreeNode(val);
+            root->left->left=ptr;
+
+            ptr=root->right;
+            root->right=new TreeNode(val);
+            root->right->right=ptr;
+        }
+
+    }
+    TreeNode* addOneRow(TreeNode* root, int val, int depth) {
+        if (depth==1){
+            TreeNode* ptr=new TreeNode(val);
+            ptr->left=root;
+            return ptr;
+        }
+        dfs(root, val, depth, 1);
+        return root;
+    }
+};
