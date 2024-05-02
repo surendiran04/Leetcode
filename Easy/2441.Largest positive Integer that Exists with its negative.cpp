13c1c8977ed08b527e04e5e8ea296c1de6d8a58d
Surendiran <137646072+surendiran04@users.noreply.github.com>
2024-05-02 23:43:58 +0530

    Update 2441.Largest positive Integer that Exists with its negative.cpp

--- Easy/2441.Largest positive Integer that Exists with its negative.cpp
+++ Easy/2441.Largest positive Integer that Exists with its negative.cpp
@@ -4,7 +4,7 @@ public:
         sort(nums.begin(),nums.end());
         for(int i=nums.size()-1;i>=0;i--){
             bool exists = find(begin(nums),end(nums), -1*nums[i]) != end(nums);
-            if(exists){
+            if(exists  && nums[i]>0){
                 return nums[i];
             }
             
