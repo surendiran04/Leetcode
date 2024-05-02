9448593dca0d4d5d5b3721e7a24a37a38628656e
Surendiran <137646072+surendiran04@users.noreply.github.com>
2024-05-02 23:42:04 +0530

    Create 2441.Largest positive Integer that Exists with its negative.cpp

---
+++ Easy/2441.Largest positive Integer that Exists with its negative.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findMaxK(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        for(int i=nums.size()-1;i>=0;i--){
+            bool exists = find(begin(nums),end(nums), -1*nums[i]) != end(nums);
+            if(exists){
+                return nums[i];
+            }
+            
+        }
+        return -1;
+    }
+};
