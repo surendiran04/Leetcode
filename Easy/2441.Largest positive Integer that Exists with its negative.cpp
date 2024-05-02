7b8a59bf13a51a8b069c26fbadc1f076866512f8
Surendiran <137646072+surendiran04@users.noreply.github.com>
2024-05-02 23:47:18 +0530

    Sort + find

--- Easy/2441.Largest positive Integer that Exists with its negative.cpp
+++ Easy/2441.Largest positive Integer that Exists with its negative.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int findMaxK(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        sort(nums.begin(),nums.end());   //T(n) - O(nlogn)
         for(int i=nums.size()-1;i>=0;i--){
-            bool exists = find(begin(nums),end(nums), -1*nums[i]) != end(nums);
+            bool exists = find(begin(nums),end(nums), -1*nums[i]) != end(nums); //returns an iterator to the end of the array if element not exist
             if(exists  && nums[i]>0){
                 return nums[i];
             }
