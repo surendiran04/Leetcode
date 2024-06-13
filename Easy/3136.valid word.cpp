c25c0c6d8ea060829142f3e352f938aa66deb57c
Surendiran <137646072+surendiran04@users.noreply.github.com>
2024-06-13 17:38:29 +0530

    Update 3136.valid word.cpp

--- Easy/3136.valid word.cpp
+++ Easy/3136.valid word.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool isValid(string word) {
+    bool isValid(string word) { //this code doesn't work
         string vowels1 = "aeiou";
         string vowels2="aeiou";
         transform(vowels2.begin(), vowels2.end(),vowels2.begin(), ::toupper);
@@ -22,6 +22,33 @@ public:
             }
             if(consonants1.find(c)!=string::npos || consonants2.find(c)!=string::npos){
                 has_consonants=true;
+            }
+        }
+        return (has_vowels && has_consonants);
+    }
+};
+//beats 100%
+class Solution {
+public:
+    bool isValid(string word) {
+        string vowels = "aeiouAEIOU";
+        string consonants = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ";
+        string digits = "0123456789";
+        string allowed = vowels + consonants + digits;
+        if (word.length() < 3) {
+            return false;
+        }
+        bool has_vowels = false;
+        bool has_consonants = false;
+        for (char c : word) {
+            if (allowed.find(c) == string::npos) {
+                return false;
+            }
+            if (vowels.find(c) != string::npos) {
+                has_vowels = true;
+            }
+            if (consonants.find(c) != string::npos) {
+                has_consonants = true;
             }
         }
         return (has_vowels && has_consonants);
