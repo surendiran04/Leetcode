0e121b4071bf3dd9ff63d75ae97adeb556f23719
Surendiran <137646072+surendiran04@users.noreply.github.com>
2024-06-25 16:29:17 +0530

    Update 2185.Counting words with a given prefix.cpp

--- Easy/2185.Counting words with a given prefix.cpp
+++ Easy/2185.Counting words with a given prefix.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    int prefixCount(vector<string>& words, string pref) { //base logic but failed for higher testcases
+    int prefixCount(vector<string>& words, string pref) {  //base logic
         int count=0;
         for (int i = 0; i < words.size();i++) {
             string cur_word = words[i];
             int k=0;
-            for (int j = 0; j < pref.length(); j++) {
+            for (int j = 0; j < pref.length() && cur_word.length()>=pref.length(); j++) {
                if(cur_word[j]==pref[j]){
                  k++;
                }
