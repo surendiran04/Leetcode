21481335b88124bb9e3b3f778accc0951493134e
Surendiran <137646072+surendiran04@users.noreply.github.com>
2024-06-29 22:43:09 +0530

    Update 182. Duplicate Emails.sql

--- SQL/182. Duplicate Emails.sql
+++ SQL/182. Duplicate Emails.sql
@@ -10,4 +10,5 @@ Each row of this table contains an email. The emails will not contain uppercase 
  
 Write a solution to report all the duplicate emails. Note that it's guaranteed that the email field is not NULL.
 Return the result table in any order
+ 
 select email from person group by email having count(email)>1;
