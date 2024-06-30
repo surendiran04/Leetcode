70973aad5d46931e2f72795042a19808c151a696
Surendiran <137646072+surendiran04@users.noreply.github.com>
2024-06-30 22:31:46 +0530

    Create 1141.User Activity for the Past 30 Days .sql

---
+++ SQL/1141.User Activity for the Past 30 Days .sql
@@ -0,0 +1,31 @@
+Write a solution to find the daily active user count for a period of 30 days ending 2019-07-27 inclusively.
+A user was active on someday if they made at least one activity on that day.
+Input: 
+Activity table:
++---------+------------+---------------+---------------+
+| user_id | session_id | activity_date | activity_type |
++---------+------------+---------------+---------------+
+| 1       | 1          | 2019-07-20    | open_session  |
+| 1       | 1          | 2019-07-20    | scroll_down   |
+| 1       | 1          | 2019-07-20    | end_session   |
+| 2       | 4          | 2019-07-20    | open_session  |
+| 2       | 4          | 2019-07-21    | send_message  |
+| 2       | 4          | 2019-07-21    | end_session   |
+| 3       | 2          | 2019-07-21    | open_session  |
+| 3       | 2          | 2019-07-21    | send_message  |
+| 3       | 2          | 2019-07-21    | end_session   |
+| 4       | 3          | 2019-06-25    | open_session  |
+| 4       | 3          | 2019-06-25    | end_session   |
++---------+------------+---------------+---------------+
+Output: 
++------------+--------------+ 
+| day        | active_users |
++------------+--------------+ 
+| 2019-07-20 | 2            |
+| 2019-07-21 | 2            |
++------------+--------------+ 
+  
+select activity_date as day,count(distinct(user_id)) as active_users
+from Activity
+where activity_date between '2019-06-28' and '2019-07-27'
+group by activity_date;
