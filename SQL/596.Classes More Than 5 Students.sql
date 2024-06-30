763a1cadea5dbc4b4e0256cce3a047955fb02d35
Surendiran <137646072+surendiran04@users.noreply.github.com>
2024-06-30 22:20:47 +0530

    Create 596.Classes More Than 5 Students.sql

---
+++ SQL/596.Classes More Than 5 Students.sql
@@ -0,0 +1,26 @@
+Write a solution to find all the classes that have at least five students.
+Input: 
+Courses table:
++---------+----------+
+| student | class    |
++---------+----------+
+| A       | Math     |
+| B       | English  |
+| C       | Math     |
+| D       | Biology  |
+| E       | Math     |
+| F       | Computer |
+| G       | Math     |
+| H       | Math     |
+| I       | Math     |
++---------+----------+
+Output: 
++---------+
+| class   |
++---------+
+| Math    |
++---------+
+  
+select class from courses
+group by class
+having count(student)>=5;
