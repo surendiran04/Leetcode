0f492ae2297d39b1bf2abcca195ec1ec37d3759b
Surendiran <137646072+surendiran04@users.noreply.github.com>
2024-06-30 22:15:41 +0530

    Create 2356.Number of Unique Subjects Taught by Each Teacher.sql

---
+++ SQL/2356.Number of Unique Subjects Taught by Each Teacher.sql
@@ -0,0 +1,25 @@
+Write a solution to calculate the number of unique subjects each teacher teaches in the university.
+Input: 
+Teacher table:
++------------+------------+---------+
+| teacher_id | subject_id | dept_id |
++------------+------------+---------+
+| 1          | 2          | 3       |
+| 1          | 2          | 4       |
+| 1          | 3          | 3       |
+| 2          | 1          | 1       |
+| 2          | 2          | 1       |
+| 2          | 3          | 1       |
+| 2          | 4          | 1       |
++------------+------------+---------+
+Output:  
++------------+-----+
+| teacher_id | cnt |
++------------+-----+
+| 1          | 2   |
+| 2          | 4   |
++------------+-----+
+  
+select teacher_id,count(Distinct(subject_id)) as cnt
+from Teacher
+group by teacher_id;
