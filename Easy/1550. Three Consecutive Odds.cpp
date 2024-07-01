b2ac2bba6772e387ecf6fe5c7c25e20b260336c8
Surendiran <137646072+surendiran04@users.noreply.github.com>
2024-07-01 21:42:46 +0530

    Ternary operator | if else | sliding window

--- Easy/1550. Three Consecutive Odds.cpp
+++ Easy/1550. Three Consecutive Odds.cpp
@@ -1,5 +1,5 @@
 //if the array consists of three consecutive odd numbers then return true else false
-class Solution {
+class Solution { //beats 60%
 public:
     bool threeConsecutiveOdds(vector<int>& arr) {
         int count=0;
@@ -11,6 +11,40 @@ public:
                 count=0;
             }
             if(count==3){
+                return true;
+            }
+        }
+        return false;
+    }
+};
+class Solution { //beats 100%
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int count=0;
+        for(int n:arr){
+            (n&1)?(count++):(count=0);  //ternary operator
+            if(count==3){
+                return true;
+            }
+        }
+        return false;
+    }
+};
+//sliding window approach
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int window = 0;
+        for (int i = 0; i < 3 && i < arr.size(); i++) { //intial window
+            window += arr[i] % 2;
+        }
+        if (window == 3) {
+            return true;
+        }
+        for (int i = 3; i < arr.size(); i++) {
+            window += arr[i] % 2;
+            window -= arr[i - 3] % 2;
+            if (window == 3) {
                 return true;
             }
         }
