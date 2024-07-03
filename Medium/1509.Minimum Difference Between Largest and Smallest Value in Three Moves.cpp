90424976710c5718b5a3dc71e86460258f8c92cb
Surendiran <137646072+surendiran04@users.noreply.github.com>
2024-07-03 21:56:01 +0530

    Update 1509.Minimum Difference Between Largest and Smallest Value in Three Moves.cpp

--- Medium/1509.Minimum Difference Between Largest and Smallest Value in Three Moves.cpp
+++ Medium/1509.Minimum Difference Between Largest and Smallest Value in Three Moves.cpp
@@ -33,3 +33,19 @@ public:
         return minVal;
     }
 };
+
+class Solution { //best approach
+public:
+    int minDifference(vector<int>& nums) {
+       if (nums.size() <= 4) {
+            return 0;
+        }
+        sort(nums.begin(), nums.end());
+        return min({
+            nums[nums.size() - 4] - nums[0],
+            nums[nums.size() - 1] - nums[3],
+            nums[nums.size() - 3] - nums[1],
+            nums[nums.size() - 2] - nums[2]
+        });
+    }
+};
