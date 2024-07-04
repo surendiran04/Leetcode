acdf585155aec0c1feb52a73d0d6398efb2e8899
Surendiran <137646072+surendiran04@users.noreply.github.com>
2024-07-04 23:14:05 +0530

    Linked List | Recursion

--- Medium/2181.Merge Nodes in Between Zeros.cpp
+++ Medium/2181.Merge Nodes in Between Zeros.cpp
@@ -1,6 +1,6 @@
-You are given the head of a linked list, which contains a series of integers separated by 0's. The beginning and end of the linked list will have Node.val == 0.
-For every two consecutive 0's, merge all the nodes lying in between them into a single node whose value is the sum of all the merged nodes. 
-The modified list should not contain any 0's.
+You are given the head of a linked list, which contains a series of integers separated by 0s. The beginning and end of the linked list will have Node.val == 0.
+For every two consecutive 0s, merge all the nodes lying in between them into a single node whose value is the sum of all the merged nodes. 
+The modified list should not contain any 0s.
 Return the head of the modified linked list.
 
   Input: head = [0,3,1,0,4,5,2,0]
