77ce8bb53ebea121c01fda818d40c1466c519001
Surendiran <137646072+surendiran04@users.noreply.github.com>
2024-07-10 23:12:57 +0530

    String | stack

--- Easy/1720-crawler-log-folder/crawler-log-folder.cpp
+++ Easy/1720-crawler-log-folder/crawler-log-folder.cpp
@@ -1,6 +1,40 @@
 class Solution {
 public:
     int minOperations(vector<string>& logs) {
+       int res = 0;
+        for (const string& log : logs) {
+            if (log == "../") {
+                if (res > 0) {
+                    res--;
+                }
+            } else if (log != "./") {
+                res++;
+            }
+        }
+        return res;
+    }
+};
+class Solution { //stack approach
+public:
+    int minOperations(vector<string>& logs) {
+        vector<string> paths_stack;
+
+        for (const string& log : logs) {
+            if (log == "../") {
+                if (!paths_stack.empty()) {
+                    paths_stack.pop_back();
+                }
+            } else if (log != "./") {
+                paths_stack.push_back(log);
+            }
+        }
+
+        return paths_stack.size();
+    }
+};
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
         int steps=0;
         int main=1;
         for(auto i:logs){
