ae50fabd33d357b50ac401f7fbf15f75f48f9224
Surendiran <137646072+surendiran04@users.noreply.github.com>
2024-07-16 23:12:31 +0530

    Time: 9 ms (45.20%) | Memory: 16.2 MB (45.07%) - LeetSync

--- Hard/1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.cpp
+++ Hard/1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.cpp
@@ -1,27 +1,16 @@
 class Solution {
 public:
     vector<string> removeAnagrams(vector<string>& words) {
-        int n=words.size();
-        for(int i=n-1;i>0;i--){
-            if(words[i-1].length()==words[i].length() && isAnagram(words[i],words[i-1])){
-                words.erase(words.begin()+i); 
-            } 
+        vector<string> ans;
+        ans.push_back(words[0]);
+        for(int i=1; i<words.size(); i++){
+            string s1 = words[i];
+            string s2 = words[i-1];
+            sort(s1.begin(), s1.end());
+            sort(s2.begin() ,s2.end());
+            if(s1!=s2)
+                ans.push_back(words[i]);
         }
-        return words;
-    }
-    bool isAnagram(string s, string t) {
-        unordered_map<char, int> count;
-        for (auto x : s) {
-            count[x]++;
-        }
-        for (auto x : t) {
-            count[x]--;
-        }
-        for (auto x : count) {
-            if (x.second != 0) {
-                return false;
-            }
-        }    
-        return true;
+        return ans;
     }
 };
